bedf4e1266d55cc476d584983f236e33a1a73e63
Simon Kiflay <szkiflay@gmail.com>
2018-08-15 23:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+// for loop
+function theBeatlesPlay(musicians, instruments){
+  var array=[];
+  for(let i=0; i< musicians.length; i++ ){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array; 
+}
+
+// while loop
+function johnLennonFacts(facts){
+  var array = [];
+  var i = 0;
+while(i < facts.length, i++){
+  array.push(`${facts}!!!`);
+  
+}
+return array;
+}
+
+// while do loop
+function iLoveTheBeatles(n){
+  var array = [];
+  do { array.push("I love the Beatles!");
+    n++;
+    } while (n<15);
+  return array;
+}
